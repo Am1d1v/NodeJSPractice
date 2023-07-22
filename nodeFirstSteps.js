fec45423314254f4849b42c19d0a2377e9b2c1a2
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-22 18:58:25 +0300

    Add files via upload
    DNS Lookup

--- nodeFirstSteps.js
+++ nodeFirstSteps.js
@@ -3,25 +3,45 @@
 // Event Loop Events 
 
 const fs = require('fs');
+const dns = require('dns');
 
+function timeStamp(){
+    return performance.now().toFixed(2);
+}
 
 console.log('Program Start');
 
+// Timeouts
 setTimeout(() => {
-    console.log('TimeOut 1', performance.now().toFixed(2))
+    process.nextTick(() => {
+        console.log('Next Tick 2', timeStamp());
+    })
+    console.log('TimeOut 1', timeStamp())
 }, 0);
 
 setTimeout(() => {
-    console.log('TimeOut 2', performance.now().toFixed(2))
+    console.log('TimeOut 2', timeStamp())
 }, 10);
 
+
+// Close Events
 fs.writeFile('./NodeBeginning.txt', 'Hello', () =>{
-    console.log('File written', performance.now().toFixed(2));
+    console.log('File written', timeStamp());
 })
 
-Promise.resolve().then(() => console.log('Promise resolved', performance.now().toFixed(2)));
+// Promises
+Promise.resolve().then(() => console.log('Promise resolved', timeStamp()));
 
-process.nextTick(() => console.log('Next tick one', performance.now().toFixed(2)));
+// Next Tick
+process.nextTick(() => console.log('Next tick one', timeStamp()));
 
+// setImmediate(Check)
+setImmediate(() => console.log('Immediate One', timeStamp()));
+
+// I/O Events
+dns.lookup('google.com', (err, address, family) =>{
+    console.log(err, address, family, timeStamp());
+    Promise.resolve().then(() => console.log('Promise 2', timeStamp()));
+});
 
 console.log('Program End');
