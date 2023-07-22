bd5c11a58968d37e41749dccdb3bdf6aae57d45f
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-22 18:16:24 +0300

    Add files via upload

--- nodeFirstSteps.js
+++ nodeFirstSteps.js
@@ -8,17 +8,20 @@ const fs = require('fs');
 console.log('Program Start');
 
 setTimeout(() => {
-    console.log('TimeOut 1')
+    console.log('TimeOut 1', performance.now().toFixed(2))
 }, 0);
 
 setTimeout(() => {
-    console.log('TimeOut 2')
+    console.log('TimeOut 2', performance.now().toFixed(2))
 }, 10);
 
 fs.writeFile('./NodeBeginning.txt', 'Hello', () =>{
-    console.log('File written');
+    console.log('File written', performance.now().toFixed(2));
 })
 
-Promise.resolve().then(() => console.log('Promise resolved'));
+Promise.resolve().then(() => console.log('Promise resolved', performance.now().toFixed(2)));
+
+process.nextTick(() => console.log('Next tick one', performance.now().toFixed(2)));
+
 
 console.log('Program End');
