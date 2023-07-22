845023cb5865bcf7c91030e78592e84d243b5be8
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-23 00:55:13 +0300

    Add files via upload

--- fib.js
+++ fib.js
@@ -1,11 +1,64 @@
 
 
+/*
+
+setTimeout(() => {
+    console.log('Timeout');
+}, 0);
+
 function fib(n){
     if( n === 0 | n ===1) {
         return n;
     }
 
     return fib(n - 1) + fib(n - 2)
+}
+
+console.log(fib(40));
+
+*/
+
+/*
+
+setTimeout(() => {
+    console.log('Timeout');
+}, 0);
+
+function fib(n){
+    return new Promise((resolve, reject) => {
+        if( n === 0 | n ===1) {
+          return  resolve(n);
+        }
+        Promise.all([fib(n - 1), fib(n - 2)])
+        .then(([fib1, fib2]) =>{
+            resolve(fib1 + fib2)
+        });
+    })
+    
+}
+
+fib(10).then((res) => console.log(res));
+
+*/
+
+
+
+function fib(n){
+    if( n === 0 | n ===1) {
+        return n;
+    }
+
+    let fib1 = 0;
+    let fib2  = 1;
+    let sum;
+
+    for(let i = 1; i < n; i++){
+        sum = fib1 + fib2;  
+        fib1 = fib2;
+        fib2 = sum;
+        //console.log(sum);
+    }
+    return sum;
 }
 
 console.log(fib(10));
